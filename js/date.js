5fa61eea079013222a2cf313682f9b386824af16
“Emelyashka” <volk.ea@bk.rugit config --global user.name Emelyashka git config --global user.email volk.ea@bk.ru>
2023-11-23 16:38:46 +0300

    task3

--- js/date.js
+++ js/date.js
@@ -8,20 +8,32 @@ function countDaysForNewYear(data)
     let NewYear=new Date(data.getFullYear()+1,0,1);
     return Math.floor((NewYear-data)/(1000*60*60*24));
 }
-function getObjectMyDays()
+function task3()
 {
-    let next,prev;
-    let data=new Date();
-    let today=data.getDay();
-    if (today==6) next=0;
-    else next=today+1;
-    if (today==0) prev=6;
-    else prev=today-1;
-    return {
-        next:next,
-        curr:today,
-        prev:prev,
-    }
+    let a = new Date(prompt("Введите дату: год-месяц-число"));
+    function getWeekDayNext(a) {
+        a.setDate(a.getDate()+1);
+        let days = ['ВС', 'ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ'];
+        return days[a.getDay()];
+        }
+    function getWeekDayCurr(a) {
+        a.setDate(a.getDate()-1);
+        console.log(a)
+        let days = ['ВС', 'ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ'];
+        return days[a.getDay()];
+        }  
+    function getWeekDayPrev(a) {
+        a.setDate(a.getDate()-1);
+        let days = ['ВС', 'ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ'];
+        return days[a.getDay()];
+        }    
+    let week = [
+        {
+          next: `${getWeekDayNext(a)}`,
+          curr:  `${getWeekDayCurr(a)}`,
+          prev: `${getWeekDayPrev(a)}`,
+        }];
+    console.log (week);
 }
 function task4()
 {
