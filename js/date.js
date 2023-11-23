3fa03008b27525ef4d5ce91404e5e33f18f004d6
“Emelyashka” <volk.ea@bk.rugit config --global user.name Emelyashka git config --global user.email volk.ea@bk.ru>
2023-11-23 15:15:59 +0300

    Задача 2 решена

--- js/date.js
+++ js/date.js
@@ -3,3 +3,8 @@ function getDayOfWeek(data)
     let days=["воскресенье", "понедельник", "вторник", "среда", "четверг", "пятница", "суббота"];
     return days[data.getDay()];
 }
+function countDaysForNewYear(data)
+{
+    let NewYear=new Date(data.getFullYear()+1,0,1);
+    return Math.floor((NewYear-data)/(1000*60*60*24));
+}
